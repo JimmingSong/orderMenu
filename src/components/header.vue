27ec190bde831811b83da5580a89d0577011f928
JimmySong <sljym0611@163.com>
2018-05-03 19:50:17 +0800

    增加头部

---
+++ src/components/header.vue
@@ -0,0 +1,32 @@
+<template>
+	<div class="head-con">
+		<div class="head-cen">
+			<div class="head-img">
+				<img src="./../assets/header.jpg" alt="图片加载失败">
+			</div>
+		</div>
+	</div>
+</template>
+<script>
+	export default {
+		name:"hd"
+	}
+</script>
+<style>
+	.head-con{
+		width: 100%;
+	}
+	.head-cen{
+		width: 1200px;
+		margin: 0 auto;
+	}
+	.head-img{
+		width: 50px;
+		height: 50px;
+	}
+	.head-img>img{
+		width: 100%;
+		height: 100%;
+		border-radius: 50%;
+	}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import test from '@/components/test'
-import btn from '@/components/button'
+import btn from '@/components/header'
 
 Vue.use(Router)
 
