9b9c5b3f52583bd320b60843bce16a6b7aa37025
JimmySong <sljym0611@163.com>
2018-05-03 18:40:45 +0800

    编辑区

---
+++ src/components/button.vue
@@ -0,0 +1,11 @@
+<template>
+	<div>
+		<el-button type="text">click me</el-button>
+		<button>click me</button>
+	</div>
+</template>
+<script>
+	export default {
+		name:"btn"
+	}
+</script>
---
+++ src/components/test.vue
@@ -0,0 +1,10 @@
+<template>
+   <div>
+      <span>这是我的一个测试路由</span>
+   </div>
+</template>
+<script>
+export default {
+   name:'test'
+}
+</script>
